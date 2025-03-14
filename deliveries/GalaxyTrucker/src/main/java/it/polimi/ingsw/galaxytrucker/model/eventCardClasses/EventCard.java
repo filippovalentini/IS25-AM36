af16b1d70db3e326ca8e1f1baf4883e61cc971dc
Filippo Valentini <pippotini02@gmail.com>
2025-03-14 20:17:47 +0100

    Creazione metodi EventCard per risoluzione carte

--- deliveries/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/model/eventCardClasses/EventCard.java
+++ deliveries/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/model/eventCardClasses/EventCard.java
@@ -6,7 +6,7 @@ import it.polimi.ingsw.galaxytrucker.model.gameClasses.GameState;
 //EVENT CARD
 //this class is used to generalize the concept of adventure/event card used during the game; each subclass of "EventCard"
 //represents a specific type of card
-public class EventCard {
+public class EventCard  {
     protected final int imageID;       //path for the image associated to the adventure card
 
     public EventCard(int imageID) {        //constructor
