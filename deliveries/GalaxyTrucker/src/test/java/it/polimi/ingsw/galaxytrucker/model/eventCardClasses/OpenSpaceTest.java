c8844bc601457fab458eade6edf6bd8a8336ff1b
Thomas Bove <thomas.bove@mail.polimi.it>
2025-03-23 19:55:53 +0100

    open space test

--- deliveries/GalaxyTrucker/src/test/java/it/polimi/ingsw/galaxytrucker/model/eventCardClasses/OpenSpaceTest.java
+++ deliveries/GalaxyTrucker/src/test/java/it/polimi/ingsw/galaxytrucker/model/eventCardClasses/OpenSpaceTest.java
@@ -33,7 +33,7 @@ class OpenSpaceTest {
         gameState.setPosition(player1, 6);
         gameState.setPosition(player2, 3);
         sides = new ArrayList<>();
-        sides.add();
+        sides.add(c);
         engine2= new Engine(false,1, sides);
         gameState.getPlayersPlay().get(player2).pickComponent(engine2);
         gameState.getPlayersPlay().get(player2).assembleComponent(1, 1);
