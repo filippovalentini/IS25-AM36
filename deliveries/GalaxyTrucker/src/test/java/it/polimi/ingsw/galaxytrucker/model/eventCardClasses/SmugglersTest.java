48af4a70474f9f82387c92184e0df5af189508d7
nicodalmonte <nicolo.dalmonte@hotmail.com>
2025-03-28 17:18:06 +0100

    test

--- deliveries/GalaxyTrucker/src/test/java/it/polimi/ingsw/galaxytrucker/model/eventCardClasses/SmugglersTest.java
+++ deliveries/GalaxyTrucker/src/test/java/it/polimi/ingsw/galaxytrucker/model/eventCardClasses/SmugglersTest.java
@@ -1,8 +1,11 @@
 package it.polimi.ingsw.galaxytrucker.model.eventCardClasses;
 
+import it.polimi.ingsw.galaxytrucker.model.componentClasses.Battery;
 import it.polimi.ingsw.galaxytrucker.model.enumerations.Color;
+import it.polimi.ingsw.galaxytrucker.model.enumerations.Connector;
 import it.polimi.ingsw.galaxytrucker.model.enumerations.State;
 import it.polimi.ingsw.galaxytrucker.model.exceptions.InvalidActionException;
+import it.polimi.ingsw.galaxytrucker.model.exceptions.NoBatteriesException;
 import it.polimi.ingsw.galaxytrucker.model.gameClasses.GameState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,6 +28,11 @@ class SmugglersTest {
         prizeGoods.add(Color.YELLOW);
         prizeGoods.add(Color.GREEN);
         prizeGoods.add(Color.BLUE);
+        List<Connector> sides = new ArrayList<>();
+        sides.add(Connector.SMOOTH);
+        sides.add(Connector.UNIVERSAL);
+        sides.add(Connector.SINGLE);
+        sides.add(Connector.DOUBLE);
         smugglers = new Smugglers(prizeGoods, 2,4, 1, 0);
         gameState = new GameState(false, 2);
         nickname = "a";
@@ -33,7 +41,9 @@ class SmugglersTest {
         gameState.addPlayer(nickname2, Color.RED);
         gameState.setPosition(nickname, 0);
         gameState.setPosition(nickname2, 1);
-        gameState.
+        Battery battery = new Battery(true,4, sides);
+
+        gameState.assembleComponent(nickname, battery, 1,3);
         gameState.setGameState(State.CARD_SOLVING);
     }
 
@@ -56,8 +66,8 @@ class SmugglersTest {
     @Test
     void testShouldNotAttackIfNotEnoughBatteries() {
         int usedBatteries = 5;
-        gameState.getNumberBatteries(nick);
+        gameState.getNumberBatteries(nickname);
         boolean looseDays = true;
-        assertThrows(InvalidActionException.class, () -> smugglers.defeat(gameState, nickname, usedBatteries, looseDays));
+        assertThrows(NoBatteriesException.class, () -> smugglers.defeat(gameState, nickname, usedBatteries, looseDays));
     }
 }
