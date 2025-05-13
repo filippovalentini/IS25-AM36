c8ca96e61b1697e42ce944bbc390268349d9feeb
nicodalmonte <nicolo.dalmonte@hotmail.com>
2025-05-13 11:29:39 +0200

    added Hourglass tests

---
+++ deliveries/GalaxyTrucker/src/test/java/it/polimi/ingsw/galaxytrucker/model/componentClasses/HourglassTest.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.galaxytrucker.model.componentClasses;
+import it.polimi.ingsw.galaxytrucker.model.exceptions.HourGlassException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class HourglassTest {
+    private Hourglass hourglass;
+
+
+    @BeforeEach
+    void init() {
+        hourglass = new Hourglass(1); // 1 seconds for testing
+    }
+
+    @Test
+    void testStartNewCycle() {
+        hourglass.startNewCycle();
+        assertEquals(1, hourglass.getNumberFlips());
+
+    }
+
+    @Test
+    void testConstructorWithInvalidDuration_Negative() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Hourglass(-2);
+        });
+        assertEquals("La durata del ciclo deve essere positiva.", exception.getMessage());
+    }
+
+    @Test
+    void testStartNewCycle_AlreadyRunning() {
+        hourglass.startNewCycle();
+        HourGlassException exception = assertThrows(HourGlassException.class, () -> {
+            hourglass.startNewCycle();
+        });
+        assertEquals("Hourglass is already running.", exception.getMessage());
+    }
+
+    @Test
+    void testStartNewCycle_AlreadyFlipped() throws InterruptedException {
+        hourglass.startNewCycle();
+        Thread.sleep(2000); // wait for the cycle to finish
+        hourglass.startNewCycle();
+        assertEquals(2, hourglass.getNumberFlips());
+    }
+    @Test
+    void testStartNewCycle_tooMuchFlips() throws InterruptedException {
+        hourglass.startNewCycle();
+        Thread.sleep(2000);
+        hourglass.startNewCycle();
+        Thread.sleep(2000);
+        HourGlassException exception = assertThrows(HourGlassException.class, () -> {
+            hourglass.startNewCycle();
+        });
+        assertEquals("Can't start a new cycle", exception.getMessage());
+
+    }
+}
+
+
+
