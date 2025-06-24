da319d4b509e2c21fcd1a7be22156e874a6631ff
nicodalmonte <nicolo.dalmonte@hotmail.com>
2025-06-24 17:40:42 +0200

    javadoc for shot classes

--- deliverables/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/model/shotClasses/CannonShot.java
+++ deliverables/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/model/shotClasses/CannonShot.java
@@ -1,18 +1,36 @@
 package it.polimi.ingsw.galaxytrucker.model.shotClasses;
 
 import it.polimi.ingsw.galaxytrucker.model.enumerations.Orientation;
-//this class describes the size and orientation of a cannon shot that can hit the ships during the game
+
+/**
+ * this class describes the size and orientation of a cannon shot that can hit the ships during the game
+ */
 public class CannonShot {
     private final boolean isLarge;      //size of the cannon shot
     private final Orientation orientation;      //direction of the cannon shot
 
+    /**
+     * Constructor for CannonShot
+     * @param isLarge
+     * @param orientation
+     */
     public CannonShot(boolean isLarge, Orientation orientation) {       //constructor
         this.isLarge = isLarge;
         this.orientation = orientation;
     }
+
+    /**
+     * Method to check if the cannon shot is large
+     * @return true if the cannon shot is large, false otherwise
+     */
     public boolean isLarge() {
         return isLarge;
     }
+
+    /**
+     * Method to get the orientation of the cannon shot
+     * @return the orientation of the cannon shot
+     */
     public Orientation getOrientation() {
         return orientation;
     }
--- deliverables/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/model/shotClasses/Meteor.java
+++ deliverables/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/model/shotClasses/Meteor.java
@@ -1,18 +1,36 @@
 package it.polimi.ingsw.galaxytrucker.model.shotClasses;
 
 import it.polimi.ingsw.galaxytrucker.model.enumerations.Orientation;
-//this class describes the size and orientation of a meteor that can hit the ships during the game
+
+/**
+ * this class describes the size and orientation of a meteor that can hit the ships during the game
+ */
 public class Meteor {
     private final boolean isLarge;      //size of the meteor
     private final Orientation orientation;      //direction of the meteor
 
+    /**
+     * Constructor for the Meteor class
+     * @param isLarge
+     * @param orientation
+     */
     public Meteor(boolean isLarge, Orientation orientation) {       //constructor
         this.isLarge = isLarge;
         this.orientation = orientation;
     }
+
+    /**
+     * Method to check if the meteor is large
+     * @return true if the meteor is large, false otherwise
+     */
     public boolean isLarge() {
         return isLarge;
     }
+
+    /**
+     * Method to get the orientation of the meteor
+     * @return the orientation of the meteor
+     */
     public Orientation getOrientation() {
         return orientation;
     }
