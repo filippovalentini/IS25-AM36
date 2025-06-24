20e0d340262e8ad6e0ed2ab6f110ab76aa04dda2
Lorenzo Bonora <lorenzo.bonora@mail.polimi.it>
2025-06-24 16:07:15 +0200

    Supporto per porte personalizzate

--- deliverables/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/MainServer.java
+++ deliverables/GalaxyTrucker/src/main/java/it/polimi/ingsw/galaxytrucker/MainServer.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.galaxytrucker;
 import it.polimi.ingsw.galaxytrucker.controller.GameController;
 import it.polimi.ingsw.galaxytrucker.network.rmi.server.ServerRMI;
 import it.polimi.ingsw.galaxytrucker.network.socket.server.SocketServer;
+import it.polimi.ingsw.galaxytrucker.ui.gui.GuiInterface;
+import it.polimi.ingsw.galaxytrucker.ui.tui.TuiInterface;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -17,10 +19,24 @@ public class MainServer {
     // the Socket and RMI servers
     public static void main(String[] args) {
         Map<Integer, GameController> controllers = new HashMap<>();
-
+        int rmiPort;
+        int socketPort;
+        if(args[1].equals("--port")){
+            if(args.length==4) {
+                rmiPort = Integer.parseInt(args[2]);
+                socketPort = Integer.parseInt(args[3]);
+            }else{
+                System.out.println("wrong arguments! using default ports...");
+                rmiPort = 1234;
+                socketPort = 1235;
+            }
+        } else { // default ports
+            rmiPort = 1234;
+            socketPort = 1235;
+        }
         new Thread(() -> {
             try {
-                startServerRMI(controllers, args[0]);
+                startServerRMI(controllers, args[0], rmiPort);
             } catch (RemoteException e) {
                 System.out.println("Error: " + e.getMessage());
             }
@@ -28,7 +44,7 @@ public class MainServer {
 
         new Thread(() -> {
             try {
-                startSocketServer(controllers);
+                startSocketServer(controllers, socketPort);
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
             }
@@ -36,20 +52,20 @@ public class MainServer {
     }
 
     //launches the socket server
-    private static void startSocketServer(Map<Integer, GameController> controllers) throws IOException {
-        ServerSocket listenSocket = new ServerSocket(1235);
+    private static void startSocketServer(Map<Integer, GameController> controllers, int port) throws IOException {
+        ServerSocket listenSocket = new ServerSocket(port);
         System.out.println("Socket server running...");
         new SocketServer(listenSocket, controllers).runServer();
     }
 
     //launches the RMI server
-    private static void startServerRMI(Map<Integer, GameController> controllers, String ipV4) throws RemoteException {
-        if(!ipV4.equals("127.0.0.1") && !ipV4.equals("localhost")) {
+    private static void startServerRMI(Map<Integer, GameController> controllers, String ipV4, int port) throws RemoteException {
+        if(!ipV4.equals("127.0.0.1") && !ipV4.equals("localhost") && !ipV4.contains("127.0.0.1:") && !ipV4.contains("localhost:")) {
             System.setProperty("java.rmi.server.hostname", ipV4);
         }
         ServerRMI server = new ServerRMI(controllers);
         final String serverName = "GalaxyTruckerServer";
-        Registry registry = LocateRegistry.createRegistry(1234);
+        Registry registry = LocateRegistry.createRegistry(port);
         registry.rebind(serverName, server);
         System.out.println("RMI server running...");
     }
